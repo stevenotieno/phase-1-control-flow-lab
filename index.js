04594ee55971d9914d5f40a3c250ac92aac9ea5e
+ <+>
2022-12-01 21:02:33 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+	// Write your code here!
+	if(distance <= 400) {
+		return 'This one is on me!';
+	}
+	else if(distance > 2000) {
+		if(distance > 2500) {
+			return 'No can do.';
+		}
+		return 'I will gladly take your thirty bucks.';
+	}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+	// Write your code here!
+	const newYorkCity = city == "NYC" ? "Ok, sounds good." : "No go.";
+	return newYorkCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+	// Write your code here!
+	let tipster;
+	switch(tip) {
+		case 'generous':
+		tipster = 'Thank you so much.';
+		break;
+		case 'not as generous':
+		tipster = 'Thank you.';
+		break;
+		case 'thanks for everything':
+		tipster = 'Bye.';
+		break;
+	}
+	return tipster;
 }
